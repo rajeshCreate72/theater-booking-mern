e0390208fc605f169951889c95358f7b6ce8b97d
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-15 15:18:33 +0530

    some changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://theater-booking-mern.vercel.app/",
+    baseURL: "https://theater-booking-mern.vercel.app",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
+    withCredentials: true,
 });
