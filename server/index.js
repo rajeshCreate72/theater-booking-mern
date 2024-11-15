2cc173252f7c060ba2a8d087925fc7694cb4c3c6
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-15 14:58:54 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL || "http://localhost:5173/", // Your client URL
+        origin: ["https://theater-booking-mern-client.vercel.app", "http://localhost:5173"],
         credentials: true,
     })
 );
