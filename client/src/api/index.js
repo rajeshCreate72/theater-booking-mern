43601698d80bd69f5d658c542e84c68b00227095
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-11-15 14:56:09 +0530

    some changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://theater-booking-mern.vercel.app/",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "http://localhost:5173/", // Your client URL
+        credentials: true,
+    })
+);
 
 dotenv.config();
 
