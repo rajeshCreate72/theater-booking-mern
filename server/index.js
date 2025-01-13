b6eb18836f751eeeacc98a9b053d5cd2f55fc431
rajeshCreate72 <mechdepartment1221@gmail.com>
2025-01-13 11:58:15 +0530

    added preflight requests

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ const theaterRoutes = require("./routes/theaterRoutes");
 const showsRoutes = require("./routes/showsRoutes");
 const bookingRoutes = require("./routes/bookingRoute");
 
+const connectDatabase = async () => {
+    try {
+        mongoose.connect(process.env.DB_URI, { serverSelectionTimeoutMS: 30000 });
+        console.log("Connected to database");
+    } catch (error) {
+        console.log("Error Connecting Database", error);
+    }
+};
+
+connectDatabase();
+
 dotenv.config();
 
 const app = express();
@@ -23,16 +34,17 @@ app.use(
     })
 );
 
-const connectDatabase = async () => {
-    try {
-        mongoose.connect(process.env.DB_URI, { serverSelectionTimeoutMS: 30000 });
-        console.log("Connected to database");
-    } catch (error) {
-        console.log("Error Connecting Database", error);
-    }
-};
+app.options("*", cors()); // Pre-flight requests for all routes
 
-connectDatabase();
+app.get("/", (req, res) => {
+    res.send(`
+    <!DOCTYPE html>
+      <html>
+        <title>Server</title>
+        <h1>Hello theater webisite</h1>
+      </html>
+    `);
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/movies", moviesRoutes);
