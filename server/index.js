7194b8e86d19babb51363edd2a72f804e4e3eff4
rajeshCreate72 <mechdepartment1221@gmail.com>
2025-01-13 12:07:22 +0530

    options added

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: "https://theater-booking-mern-client.vercel.app/",
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
